aad215f1f27b0f59d40227fff00d508325043f05
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-07 18:59:26 -0300

    agregando la url

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://task-manager-alpha-brown.vercel.app/',
+    credentials: true
+}))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
