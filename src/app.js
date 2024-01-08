a7f715ed1b2e4b74fab635775937d47acb1b9e5b
federicoherrera <fede.juan.herrera@gmail.com>
2024-01-08 20:13:06 -0300

    g

--- src/app.js
+++ src/app.js
@@ -7,12 +7,6 @@ import cors from 'cors'
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://task-manager-alpha-brown.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 
 app.get('/', (req, res) => {
     res.json('Hola mundo')
